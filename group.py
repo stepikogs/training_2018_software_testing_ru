9bda3e454a9d764fea201354b75aaebc58fccaa1
George Stepiko <stepikogs@gmail.com>
2018-06-18 06:23:00 -0700

    base classes extracted

---
+++ group.py
@@ -0,0 +1,9 @@
+__author__ = 'George Stepiko'
+
+
+class Group:
+
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
