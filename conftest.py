f6b25d609d49849c1351fe38a0fa4c965bf73842
George Stepiko <stepikogs@gmail.com>
2018-07-16 22:30:50 -0700

    stepiko task16 - parametrization
    + new '--browser' parameter with 'chrome' as default value (just for my comfort =)
    + new '--baseUrl' parameter with 'http://localhost/addressbook/' as default value
    
    Signed-off-by: George Stepiko <stepikogs@gmail.com>

--- conftest.py
+++ conftest.py
@@ -8,10 +8,12 @@ fixture = None
 @pytest.fixture  # (scope='session')
 def app(request):
     global fixture
+    browser = request.config.getoption('--browser')
+    base_url = request.config.getoption('--baseUrl')
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     elif not fixture.is_valid():
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -23,3 +25,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(finik)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome')
+    parser.addoption('--baseUrl', action='store', default='http://localhost/addressbook/')
--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 __author__ = 'George Stepiko'
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.record import RecordHelper
@@ -8,11 +8,19 @@ from fixture.record import RecordHelper
 class Application:
 
     # fixture methods
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+    def __init__(self, browser, base_url):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError('unrecognized browser %s' % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.record = RecordHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -30,7 +38,7 @@ class Application:
         wd = self.wd
         if wd.current_url.endswith('/addressbook/') and wd.find_elements_by_xpath('//img[@title="Details"]'):
             return
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def return_to_home_page(self):
         wd = self.wd
