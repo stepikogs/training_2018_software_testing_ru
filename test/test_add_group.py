142f9d94aa95d69d92493b431ef9f171f7a828e6
George Stepiko <stepikogs@gmail.com>
2018-07-20 17:59:38 -0700

    stepiko interim - test data in py file
    Signed-off-by: George Stepiko <stepikogs@gmail.com>

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,32 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlength):
-    symbols = string.ascii_letters + string.digits  # + ' '*10 fixme: work with multi-space
-    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlength))])
-
-
-testdata = [
-    Group(group_name="testgroup",                    # full group
-          group_header="testheader",
-          group_footer="testfooter"),
-    Group(),                                         # default group
-    Group(group_name="name_assigned",                # group with invalid input
-          jeegurda='incorrect',
-          group_header='header_assigned'),
-    Group(group_name="",                             # group with empty values
-          group_header="",
-          group_footer="")
-    ] + [
-    Group(group_name=random_string('name', 10),      # group with random values
-          group_footer=random_string('footer', 10),
-          group_header=random_string('header', 10))
-    for i in range(5)                                # 5 random groups in total
-    ]
+from data.add_group import constant as testdata
 
 
 # test methods
