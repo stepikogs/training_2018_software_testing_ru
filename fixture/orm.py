b825c2e7467c4a7603fdd445c798ca1ba5910acf
George Stepiko <stepikogs@gmail.com>
2018-08-09 22:45:57 -0700

    stepiko interim - poni orm  som extra methods
    + get_records_in_group
    + get_record_not_in_group
    * converters updated
    
    Signed-off-by: George Stepiko <stepikogs@gmail.com>

--- fixture/orm.py
+++ fixture/orm.py
@@ -4,7 +4,8 @@ from pony.orm import *
 from datetime import datetime
 from model.group import Group
 from model.record import Record
-from pymysql.converters import decoders
+# from pymysql.converters import decoders
+from pymysql.converters import encoders, decoders, convert_mysql_timestamp
 
 
 class OrmFixture:
@@ -16,21 +17,29 @@ class OrmFixture:
         group_name = Optional(str, column='group_name')
         group_header = Optional(str, column='group_header')
         group_footer = Optional(str, column='group_footer')
+        records = Set(lambda: OrmFixture.OrmRecord, table='address_in_groups', column='id',
+                      reverse='groups', lazy=True)
 
     class OrmRecord(db.Entity):
         _table_ = 'addressbook'
         id = PrimaryKey(int, column='id')
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
-        deprecated = Optional(str, column='deprecated')  # datetime or string?
+        deprecated = Optional(str, column='deprecated')
+        groups = Set(lambda: OrmFixture.OrmGroup, table='address_in_groups', column='group_id',
+                     reverse='records', lazy=True)
 
     def __init__(self, host, name, user, password):
+        conv = encoders
+        conv.update(decoders)
+        conv[datetime] = convert_mysql_timestamp
         self.db.bind('mysql',
                      host=host,
                      database=name,
                      user=user,
                      password=password,
-                     conv=decoders
+                     conv=conv
+                     # conv=decoders
                      )
         self.db.generate_mapping()
 
@@ -56,3 +65,14 @@ class OrmFixture:
     @db_session
     def get_record_list(self):
         return self.convert_records_to_model(select(r for r in OrmFixture.OrmRecord if r.deprecated is None))
+
+    @db_session
+    def get_records_in_group(self, group):
+        orm_group = list(select(g for g in OrmFixture.OrmGroup if g.id == group.id))[0]
+        return self.convert_records_to_model(orm_group.records)
+
+    @db_session
+    def get_records_not_in_group(self, group):
+        orm_group = list(select(g for g in OrmFixture.OrmGroup if g.id == group.id))[0]
+        return self.convert_records_to_model(select(r for r in OrmFixture.OrmRecord
+                                                    if r.deprecated is None and orm_group not in r.groups))
--- test/check_db.py
+++ test/check_db.py
@@ -2,11 +2,12 @@ __author__ = 'George Stepiko'
 
 
 from fixture.orm import OrmFixture
+from model.group import Group
 
 db = OrmFixture(host='127.0.0.1', name='addressbook', user='root', password='')
 
 try:
-    lala = db.get_record_list()
+    lala = db.get_records_not_in_group(Group(id='118'))
     for item in lala:
         print(item)
     print(len(lala))
