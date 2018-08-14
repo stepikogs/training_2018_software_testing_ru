d26263b3e208e81f20bb710ff295f8f18f3297df
George Stepiko <stepikogs@gmail.com>
2018-08-14 08:52:31 -0700

    stepiko task 22 FIX - add/delete record to group: two tests
    * the only tests split to two with pre-conditions updated
    
    Signed-off-by: George Stepiko <stepikogs@gmail.com>

--- test/test_record_in_group.py
+++ test/test_record_in_group.py
@@ -5,7 +5,7 @@ from fixture.orm import OrmFixture
 orm_base = OrmFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
-def test_record_with_group(app, db):
+def test_add_record_to_group(app, db):
     # preconditions: 1 group and 1 record required
     app.group.provide()
     app.record.provide()
@@ -13,17 +13,33 @@ def test_record_with_group(app, db):
     acceptor = choice(db.get_group_list())
     # get records and take one
     donor = choice(db.get_record_list())
-    print(donor)
     # get current 'in group' records (supposed to be empty but...)
     in_group = orm_base.get_records_in_group(acceptor)
-    assert donor not in in_group  # is False
-    # add record to group
+    # provide record NOT in group before test
+    if donor in in_group:
+        app.record.remove_record_from_group_by_ids(record=donor, group=acceptor)
+    # TEST: add record to group
     app.record.add_record_to_group_by_ids(record=donor, group=acceptor)
     # assertions
     in_group = orm_base.get_records_in_group(acceptor)
     assert donor in in_group  # is True
-    # postconditions: delete it from group
+
+
+def test_remove_record_from_group(app, db):
+    # preconditions: 1 group and 1 record required
+    app.group.provide()
+    app.record.provide()
+    # get groups ant take random one
+    acceptor = choice(db.get_group_list())
+    # get records and take one
+    donor = choice(db.get_record_list())
+    # get current 'in group' records (supposed to be empty but...)
+    in_group = orm_base.get_records_in_group(acceptor)
+    # provide record NOT in group before test
+    if donor not in in_group:
+        app.record.add_record_to_group_by_ids(record=donor, group=acceptor)
+    # pTEST: remove record from group
     app.record.remove_record_from_group_by_ids(record=donor, group=acceptor)
-    # postcondition assertions
+    # assertion
     in_group = orm_base.get_records_in_group(acceptor)
     assert donor not in in_group  # is False
