d85e8b7d667c9ad2e6829097672a9d4b803d2a49
George Stepiko <stepikogs@gmail.com>
2018-08-18 21:35:49 -0700

    stepiko interim
    + confog screenshot
    * delete_first_group test fixed - it must not use db as test subject is first group in WEB list
    
    Signed-off-by: George Stepiko <stepikogs@gmail.com>

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -18,17 +18,15 @@ def test_delete_random_group(app, db, check_ui):
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_list(), key=Group.id_or_max)
 
 
-def test_delete_first_group(app, db, check_ui):
+# we can not use db here as we work with the first group in WEB!
+def test_delete_first_group(app):
     app.group.provide()
-    old_groups = db.get_group_list()
+    old_groups = app.group.get_list()
     app.group.delete_first()
-    new_groups = db.get_group_list()
+    new_groups = app.group.get_list()
     assert len(old_groups) - 1 == len(new_groups)
     old_groups[0:1] = []
-    assert old_groups == new_groups
-    if check_ui:
-        # fixme does not work with multi and traveling spaces
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_list(), key=Group.id_or_max)
+    assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_list(), key=Group.id_or_max)
 
 
 def test_delete_all_groups(app, db, check_ui):
